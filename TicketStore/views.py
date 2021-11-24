62281f0f4cab522eb2b64c265ab044209e050079
taras-fid <taras.fid@gmail.com>
2021-11-24 23:50:28 +0200

    123

--- TicketStore/views.py
+++ TicketStore/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Ticket
 # Create your views here.
+
+
+def first_ticket_from_tickets(request):
+    first_ticket = Ticket.objects.first()
+    context = {'first_ticket': first_ticket}
+    return render(request)
