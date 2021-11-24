8abc4f6cad10a0eb1323cc538af99b89d9170ef6
Monsotya <sonya20033009@gmail.com>
2021-11-24 21:22:51 +0200

    Models` fix and made migration

--- performance/models.py
+++ performance/models.py
@@ -3,41 +3,39 @@ from django.db import models
 import django.core.validators as validators
 
 
-# Create your models here.
-
-
 class Rating(models.Model):
-    description = models.TextField()
-    min_age = models.IntegerField()
+    description = models.TextField('Description of a rating', max_length=100)
+    min_age = models.IntegerField('Minimal age of a visitor')
+
+
+class Genre(models.Model):
+    genre = models.TextField('Name of genre', max_length=32)
 
 
 class Performance(models.Model):
-    name = models.CharField(max_length=64)
-    rating_id = models.ForeignKey(Rating, on_delete=models.SET_NULL)
-    description = models.TextField
-    author = models.CharField(max_length=64)
-    duration = models.DurationField()
-    genre_id = models.ForeignKey(Genre, on_delete=models.SET_NULL)
-    price = models.DecimalField(validators=[
-        validators.MinValueValidator(0.01),
-        validators.DecimalValidator(decimal_places=2)
+    name = models.CharField('Name', max_length=64)
+    rating_id = models.ForeignKey(Rating, null=True, on_delete=models.SET_NULL)
+    description = models.TextField('Description of performance')
+    author = models.CharField('Author', max_length=64)
+    duration = models.DurationField('Duration of performance')
+    genre_id = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+    price = models.DecimalField('Price', decimal_places=2, max_digits=10, validators=[
+        validators.MinValueValidator(0.01)
     ])
 
-class Genre(models.Model):
-    genre = models.TextField(max_length=32)
 
 class Hall(models.Model):
-    number = models.CharField(max_length=32)
-    description = models.TextField
+    number = models.CharField('Number of a hall', max_length=32)
+    description = models.TextField('Description', max_length=100)
 
 
 class Tier(models.Model):
-    hall_id = models.ForeignKey(Hall, on_delete=models.CASCADE)
-    level = models.IntegerField()
-    number_of_seats = models.PositiveIntegerField()
+    hall_id = models.ForeignKey(Hall, null=True, on_delete=models.CASCADE)
+    level = models.IntegerField('Level of tier')
+    number_of_seats = models.PositiveIntegerField('Number of seats')
 
 
 class Poster(models.Model):
-    performance_id = models.ForeignKey(Performance, on_delete=models.SET_NULL)
-    date = models.DateTimeField()
-    hall_id = models.ForeignKey(Hall, on_delete=models.SET_NULL)
+    performance_id = models.ForeignKey(Performance, null=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField('Date of performance')
+    hall_id = models.ForeignKey(Hall, null=True, on_delete=models.SET_NULL)
