d990ce600189a91c2142fc0729e6305e84e18580
taras-fid <taras.fid@gmail.com>
2021-12-25 16:27:18 +0200

    123

--- ticketStore/views.py
+++ ticketStore/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Ticket
-from performance.models import Performance
+from performance.models import Performance, Poster
 from django.urls import reverse_lazy
 from django.db.models import Q
 from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.base import View
+from datetime import date
 
 
 # Create your views here.
@@ -26,13 +27,19 @@ def ticketStore_delete(request):
 
 
 def ticketStore_hot(request):
-    return render(request, 'ticketStore/ticketStore_hot.html')
+    today = date.today()
+    d = today.strftime("%Y/%m/%d")
+    performances = Performance.objects.order_by('-price')
+    poster = Poster.objects.order_by('id')
+    return render(request, 'ticketStore/ticketStore_hot.html', {'performances': performances, 'poster': poster, 'date': d})
 
 
-def ticketStore_1(request):
+
+def ticketStore_performance(request, pk):
+    performances = Performance.objects
     tickets = Ticket.objects.order_by('availability', 'place')
     # for el in tickets:
     #     if not int(el.place) % 10:
     #         i = 0
-    data = {'tickets': tickets}
-    return render(request, 'ticketStore/ticketStore_1.html', context=data)
+    data = {'tickets': tickets, 'performances': performances}
+    return render(request, 'ticketStore/ticketStore_performance.html', context=data)
